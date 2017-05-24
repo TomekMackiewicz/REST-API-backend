948f80ce634084619d45806c5621723a80ce1fcb
Tomasz Mackiewicz <Tomasz.Mackiewicz@altkom.pl>
2017-05-24 11:49:53 +0200

    Finished api config

--- src/AppBundle/Entity/BlogPost.php
+++ src/AppBundle/Entity/BlogPost.php
@@ -3,10 +3,12 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMSSerializer;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\BlogPostRepository")
  * @ORM\Table(name="blog_post")
+ * @JMSSerializer\ExclusionPolicy("all")
  */
 class BlogPost implements \JsonSerializable {
 
@@ -14,37 +16,26 @@ class BlogPost implements \JsonSerializable {
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @JMSSerializer\Expose
      */
     protected $id;
 
     /**
      * @ORM\Column(type="string", name="title")
+     * @JMSSerializer\Expose
      */
-    protected $title;
+    private $title;
 
     /**
      * @ORM\Column(type="string", name="body")
+     * @JMSSerializer\Expose
      */
-    protected $body;
+    private $body;
 
-//    /**
-//     * @ORM\Column(name="decimal_1", type="decimal", scale=2, precision=8)
-//     */
-//    protected $decimal1;
-//
-//    /**
-//     * @ORM\Column(name="decimal_2", type="decimal", scale=3, precision=8)
-//     */
-//    protected $decimal2;
-//
-//    /**
-//     * @ORM\Column(name="decimal_3", type="decimal",  precision=10, scale=4)
-//     */
-//    protected $decimal3;
-
+    /**
+     * @return int
+     */
     public function getId() {
-        // this has the word private in it
-        // and a semi colon ; for some reason
         return $this->id;
     }
 
--- src/AppBundle/Form/Type/BlogPostType.php
+++ src/AppBundle/Form/Type/BlogPostType.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,13 +17,13 @@ class BlogPostType extends AbstractType {
         $builder
                 ->add('title', TextType::class)
                 ->add('body', TextType::class)
-                ->add('submit', SubmitType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             'data_class' => 'AppBundle\Entity\BlogPost',
+            'allow_extra_fields' => true,
         ]);
     }
 
