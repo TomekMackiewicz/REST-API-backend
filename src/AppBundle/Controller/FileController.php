9ba42fc2c7f0cbdc1b23ce83490ebefd7753f15a
root <tomasz.mackiewicz79@gmail.com>
2017-07-14 13:45:34 +0200

    Get files method

--- src/AppBundle/Controller/FileController.php
+++ src/AppBundle/Controller/FileController.php
@@ -7,9 +7,38 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\Finder\Finder;
+//use FOS\RestBundle\Controller\FOSRestController;
 
 class FileController extends Controller implements ClassResourceInterface
 {
+    
+    public function cgetAction() 
+    {
+        $finder = new Finder();
+        $finder->files()->in('/var/www/rest-api/REST-API-backend/web/uploads');
+        //$root1 = $this->container->get('app.root.service');
+        //$root2 = $root1->getProjectDir();
+        //$finder->directories()->in(__DIR__);
+        
+        $files = [];
+        
+        foreach ($finder as $file) {
+            $files[] = $file->getRelativePathname();
+            // Dump the absolute path
+            //var_dump($file->getRealPath());
+            //file_put_contents('/var/www/log.log', $file->getRealPath());
+
+            // Dump the relative path to the file, omitting the filename
+            //var_dump($file->getRelativePath());
+
+            // Dump the relative path to the file
+            //var_dump($file->getRelativePathname());
+        }
+        
+        return $files;
+    }    
+    
     public function postAction(Request $request)
     { 
         $uploadHandler = $this->get('srio_rest_upload.upload_handler');
