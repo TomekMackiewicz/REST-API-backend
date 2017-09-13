46dfe00c7506f8bced22434747ce0b51d9f212f5
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2017-09-13 21:57:45 +0200

    Finished place payu order

--- src/AppBundle/Controller/PaymentController.php
+++ src/AppBundle/Controller/PaymentController.php
@@ -7,7 +7,9 @@ use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use Symfony\Component\HttpFoundation\Request;
 //use Symfony\Component\HttpFoundation\Response;
-use Unirest;
+//use Unirest;
+use OpenPayU_Configuration;
+use OpenPayU_Order;
 
 /**
  * Class PaymentController
@@ -24,143 +26,37 @@ class PaymentController extends FOSRestController implements ClassResourceInterf
      * @param Request $request
      *
      */
-    public function postAction(Request $request) {        
-        $form = json_decode($request->getContent(), TRUE);
-        $secondKey = 'f289568f7d7937e9168519f17217f07d';
-        $algorithm = 'SHA-256';
-        $posId = "302325";        
-                
-        $auth = Unirest\Request::post(
-            'https://secure.snd.payu.com/pl/standard/user/oauth/authorize', 
-            $headers = array(), 
-            $body = 'grant_type=client_credentials&client_id=302325&client_secret=826745237794f7fd98a0f4e6ca5a38e2'   
-        );              
-        $arr2 = [
-            //"notifyUrl" => "https://google.com",
-            "notifyUrl" => "http://shop.url/notify",
-            "customerIp" => "127.0.0.1",
-            //"customerIp" => "123.123.123.123",
-            //"continueUrl" => "https://google.com",
-            "continueUrl" => "http://shop.url/continue",
-            "merchantPosId" => $posId,
-            //"merchantPosId" => 145227,
-            "description" => "RTVmarket",
-            "currencyCode" => "PLN",
-            "totalAmount" => "10000",
-            //"extOrderId" => "xakcvih7tjamkw2ga106ul",
-            //"OpenPayu-Signature" => "sender=145227;algorithm=SHA-256;signature=bc94a8026d6032b5e216be112a5fb7544e66e23e68d44b4283ff495bdb3983a8"
-            //"OpenPayu-Signature" => $signature
-        ];
-        $arr3 = array_merge($arr2, $form);  
+    public function postAction(Request $request) { 
 
-        $signature = $this->generateSignData($arr3, $algorithm, $posId, $secondKey);
+        OpenPayU_Configuration::setEnvironment('sandbox');
+        OpenPayU_Configuration::setMerchantPosId('302325');
+        OpenPayU_Configuration::setSignatureKey('f289568f7d7937e9168519f17217f07d');
+        OpenPayU_Configuration::setOauthClientId('302325');
+        OpenPayU_Configuration::setOauthClientSecret('826745237794f7fd98a0f4e6ca5a38e2');        
         
-        $arr4 = [
-            "OpenPayu-Signature" => $signature
-        ];
+        $data = json_decode($request->getContent(), TRUE);       
         
-        $arr5 = array_merge($arr3, $arr4);
-        
-        $headers = array(
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer ' .$auth->body->access_token
-        );
+        $order['continueUrl'] = 'http://localhost:4200';
+        $order['notifyUrl'] = 'http://localhost:4200';
+        $order['customerIp'] = $_SERVER['REMOTE_ADDR'];
+        $order['merchantPosId'] = OpenPayU_Configuration::getMerchantPosId();
+        $order['description'] = 'New order';
+        $order['currencyCode'] = 'PLN';
+        $order['totalAmount'] = 10000;
+        //$order['extOrderId'] = '1342'; //must be unique!
 
-        $body = Unirest\Request\Body::json($arr5);
-        
-        $response = Unirest\Request::post('https://secure.snd.payu.com/api/v2_1/orders', $headers, $body);
-        //$response = Unirest\Request::post('https://secure.payu.com/api/v2_1/orders', $headers, $body);
-//        ob_start();
-//        print_r($body);
-//        $textualRepresentation = ob_get_contents();
-//        ob_end_clean();
-//        file_put_contents('/var/www/log.log', $textualRepresentation);              
+        $order['products'][0]['name'] = $data['products'][0]['name'];
+        $order['products'][0]['unitPrice'] = $data['products'][0]['unitPrice'];
+        $order['products'][0]['quantity'] = $data['products'][0]['quantity'];
 
-        return $response;
+        $order['buyer']['email'] = $data['buyer']['email'];
+        $order['buyer']['phone'] = $data['buyer']['phone'];
+        $order['buyer']['firstName'] = $data['buyer']['firstName'];
+        $order['buyer']['lastName'] = $data['buyer']['lastName'];        
+        
+        $response = OpenPayU_Order::create($order);
+
+        return $response->getResponse()->redirectUri;
     }
-
-    /**
-     * Function generate sign data
-     *
-     * @param array $data
-     * @param string $algorithm
-     * @param string $merchantPosId
-     * @param string $signatureKey
-     *
-     * @return string
-     *
-     * @throws OpenPayU_Exception_Configuration
-     */
-    public static function generateSignData(array $data, $algorithm = 'SHA-256', $merchantPosId = '', $signatureKey = '')
-    {
-        if (empty($signatureKey))
-            throw new OpenPayU_Exception_Configuration('Merchant Signature Key should not be null or empty.');
-        if (empty($merchantPosId))
-            throw new OpenPayU_Exception_Configuration('MerchantPosId should not be null or empty.');
-        $contentForSign = '';
-
-        //$aamount = ['totalAmount' => $form['totalAmount']];
-        //$abuyer = $form['buyer'];
-        //$aproducts = $form['products'];
-        //$flatten = array_merge($aamount, $abuyer, $aproducts);
-$test = self::flatten($data);
-        
-        ksort($test);       
-
-        ob_start();
-        print_r($test);
-        $textualRepresentation = ob_get_contents();
-        ob_end_clean();
-        file_put_contents('/var/www/log.log', $textualRepresentation);  
-        
-//        foreach ($data as $key => $value) {
-//            $contentForSign .= $key . '=' . urlencode($value) . '&';                
-//        }
-        
-$contentForSign = 'buyer[email]=tomek@gmail.com&'
-        . 'buyer[firstName]=tomek&'
-        . 'buyer[lastName]=Mackiewicz&'
-        . 'buyer[phone]=111111111&'
-        . 'continueUrl=http://shop.url/continue&'
-        . 'currencyCode=PLN&'
-        . 'customerIp=127.0.0.1&'
-        . 'description=RTVmarket&'        
-        . 'merchantPosId=302325&'        
-        . 'notifyUrl=http://shop.url/notify&'        
-        . 'products[0][name]=legalForm&'        
-        . 'products[0][unitPrice]=10000&'
-        . 'products[0][quantity]=1&'        
-        . 'totalAmount=10000'
-        ;
-
-        
-        if (in_array($algorithm, array('SHA-256', 'SHA'))) {
-            $hashAlgorithm = 'sha256';
-            $algorithm = 'SHA-256';
-        } else if ($algorithm == 'SHA-384') {
-            $hashAlgorithm = 'sha384';
-            $algorithm = 'SHA-384';
-        } else if ($algorithm == 'SHA-512') {
-            $hashAlgorithm = 'sha512';
-            $algorithm = 'SHA-512';
-        }
-        $signature = hash($hashAlgorithm, $contentForSign . $signatureKey);
-        $signData = 'sender=' . $merchantPosId . ';algorithm=' . $algorithm . ';signature=' . $signature;
-       
-        return $signData;
-    }
-
-private function flatten($array, $prefix = '') {
-    $result = array();
-    foreach($array as $key=>$value) {
-        if(is_array($value)) {
-            $result = $result + flatten($value, $prefix . $key . '.');
-        }
-        else {
-            $result[$prefix . $key] = $value;
-        }
-    }
-    return $result;
-}
     
 }
