04079dc6a3072f8a6e3c0e93bed1634a6476af6e
root <tomasz.mackiewicz79@gmail.com>
2017-09-14 10:41:09 +0200

    Payment form

---
+++ src/AppBundle/Form/Type/PaymentType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('totalAmount', TextType::class)
+            ->add('unitPrice', TextType::class)                
+            ->add('quantity', TextType::class)                
+            ->add('name', TextType::class)                
+            ->add('email', TextType::class)                
+            ->add('phone', TextType::class)                
+            ->add('firstName', TextType::class)                
+            ->add('lastName', TextType::class);         
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Payment',
+            'allow_extra_fields' => true,
+        ]);
+    }
+
+    public function getName() {
+        return 'payment';
+    }
+
+}
